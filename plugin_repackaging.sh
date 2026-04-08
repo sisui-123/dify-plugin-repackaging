f77717909d7969f081dfe38803b5884771168f56
Wang Qinghui <118579656+sisui-123@users.noreply.github.com>
2026-04-08 11:21:12 +0800

    Update plugin_repackaging.sh

--- plugin_repackaging.sh
+++ plugin_repackaging.sh
@@ -419,7 +419,7 @@ while getopts "p:s:R" opt; do
 	case "$opt" in
 		p)
 			RAW_PLATFORM="${OPTARG}"
-			PIP_PLATFORM="--platform ${OPTARG} --only-binary=:all: --no-binary=docopt"
+			PIP_PLATFORM="--platform ${OPTARG} --only-binary=:all: --no-binary=:none:"
 			;;
 		s) PACKAGE_SUFFIX="${OPTARG}" ;;
 		R) PRERELEASE_ALLOW=1 ;;
